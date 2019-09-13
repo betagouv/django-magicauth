5beadb6195a6629b1d13299a4c94bf1a84ea1eee
Anna-Livia <annalivia.gomart@gmail.com>
2019-09-13 11:28:13 +0200

    Add testing fixtures

---
+++ magicauth/tests/test_url.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("", include("magicauth.urls")),
+    path("test_home/", TemplateView.as_view(template_name='home.html'), name='home')
+]
