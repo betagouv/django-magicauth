fd7557a85f96c2ca7fd576af6de013510f59ad63
Anna-Livia <annalivia.gomart@gmail.com>
2019-10-05 12:21:20 +0200

    Pass the context from GET to POST

--- magicauth/views.py
+++ magicauth/views.py
@@ -26,10 +26,14 @@ class LoginView(FormView):
             "LOGGED_IN_REDIRECT_URL_NAME"
         ] = magicauth_settings.LOGGED_IN_REDIRECT_URL_NAME
         context["LOGOUT_URL_NAME"] = magicauth_settings.LOGOUT_URL_NAME
+        context["next_view"] = self.request.GET.get(
+            "next", f"/{magicauth_settings.LOGGED_IN_REDIRECT_URL_NAME}/"
+        )
         return context
 
-    def form_valid(self, form):
-        form.send_email(self.request)
+    def form_valid(self, form, *args, **kwargs):
+        next_view = self.get_context_data().get("next_view")
+        form.send_email(self.request, next_view)
         return super().form_valid(form)
 
 
