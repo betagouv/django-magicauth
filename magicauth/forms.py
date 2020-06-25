62e4d1ec43575cb670ea7a32aac480c4ae71517f
Sylvain <sylvain@lazybird.co>
2020-06-25 15:23:30 +0200

    Remove unused send email code from the form - it's now done elsewhere.

--- magicauth/forms.py
+++ magicauth/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.core.mail import send_mail
-from django.template import loader
 from django.utils.module_loading import import_string
 from magicauth import settings as magicauth_settings
-from magicauth.models import MagicToken
-import math
+
 
 email_unknown_callback = import_string(magicauth_settings.EMAIL_UNKNOWN_CALLBACK)
 
@@ -22,36 +19,3 @@ class EmailForm(forms.Form):
         if not get_user_model().objects.filter(**field_lookup).exists():
             email_unknown_callback(user_email)
         return user_email
-
-    def create_token(self, user):
-        token = MagicToken.objects.create(user=user)
-        return token
-
-    def send_email(self, current_site, next_url):
-        user_email = self.cleaned_data["email"]
-        email_field = magicauth_settings.EMAIL_FIELD
-        field_lookup = {f"{email_field}__iexact": user_email}
-        user = get_user_model().objects.get(**field_lookup)
-        token = self.create_token(user)
-        email_subject = magicauth_settings.EMAIL_SUBJECT
-        html_template = magicauth_settings.EMAIL_HTML_TEMPLATE
-        text_template = magicauth_settings.EMAIL_TEXT_TEMPLATE
-        from_email = magicauth_settings.FROM_EMAIL
-        context = {
-            "token": token,
-            "user": user,
-            "site": current_site,
-            "next_url": next_url,
-            "TOKEN_DURATION_MINUTES": math.floor(magicauth_settings.TOKEN_DURATION_SECONDS / 60),
-            "TOKEN_DURATION_SECONDS": magicauth_settings.TOKEN_DURATION_SECONDS,
-        }
-        text_message = loader.render_to_string(text_template, context)
-        html_message = loader.render_to_string(html_template, context)
-        send_mail(
-            subject=email_subject,
-            message=text_message,
-            from_email=from_email,
-            html_message=html_message,
-            recipient_list=[user_email],
-            fail_silently=False,
-        )
